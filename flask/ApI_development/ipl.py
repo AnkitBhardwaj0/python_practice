f54c330f57a9b08297db1f4613c0a92c6acd525b
Ankit <ankitrehua1@gmail.com>
2026-08-28 03:49:52 +0530

    solve some mistake

--- flask/ApI_development/ipl.py
+++ flask/ApI_development/ipl.py
@@ -12,12 +12,12 @@ def teamAPI():
      return teams_dict
 def teamvteamAPI(team1,team2):
     valid_team=teamAPI()
-    if team1 in valid_team and team2 in valid_team:
+    if team1 in valid_team['teams'] and team2 in valid_team['teams']:
          team_df=matches[(matches['Team1']==team1) & (matches['Team2']==team2) | (matches['Team1']==team2) & (matches['Team2']==team1)]
          total_matches=team_df.shape[0]
 
-         matches_won_team1=team_df['WinningTeam'].value_counts()[team1]
-         matches_won_team2=team_df['WinningTeam'].value_counts()[team2]
+         matches_won_team1=team_df['WinningTeam'].value_counts().get(team1,0)
+         matches_won_team2=team_df['WinningTeam'].value_counts().get(team2,0)
          draws=total_matches-(matches_won_team1+matches_won_team2)
          response={
              'total_matches':str(total_matches),
